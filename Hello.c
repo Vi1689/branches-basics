ee15b682f864bd9b6bd551e0c65bda8f6dfda279
Баранов Виталий <baranov2008@ngs.ru>
2023-04-01 12:38:42 +0700

    Hello

---
+++ Hello.c
@@ -0,0 +1,10 @@
+#include "Hello.h"
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	print(a);
+	return 0;
+}
---
+++ Hello.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void print(int a)
+{
+	for(int i = 0; i < a; ++i)
+	{
+		printf("Hello!\n");
+	}
+}
